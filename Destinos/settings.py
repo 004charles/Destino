454c3c564a1f487e7e3db1d90d78463faf7e8804
TheresaChruz <theresachruz@gmail.com>
2024-09-07 08:37:58 +0100

    Update settings.py

--- Destinos/settings.py
+++ Destinos/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'usuarios',
+    'django_filters',
     'rest_framework',
     'rest_framework.authtoken',
 ]
